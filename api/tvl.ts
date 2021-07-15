019a55520a1f91de2ed357f2f220158742f2e3ce
Jitendra Singh <jitendra@eiysys.in>
2021-07-15 12:27:57 +0530

    tvl api create

---
+++ api/tvl.ts
@@ -0,0 +1,62 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { return200, return500 } from "../utils/response";
+import  axios  from 'axios';
+import { getAddress } from "@ethersproject/address";
+import { getTopPairs } from "../utils";
+interface ReturnShape {
+  [tokenIds: string]: {
+    price: string;
+    base_volume: string;
+    quote_volume: string;
+    liquidity: string;
+    liquidity_BNB: string;
+  };
+}
+export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
+  try {
+    axios.get('https://cashcow-frontend.vercel.app/api/pairs')
+  .then(result => {
+
+    let total_liq_bnb=0;
+    let allPairsData = result.data.data;
+    let filterdata=[];
+    let amount:any =0;
+  for(var key in allPairsData){
+    var keyjson = allPairsData[key];
+    filterdata.push(keyjson)
+  }
+  console.log("data" , filterdata)
+  for(let i=0; i<filterdata.length; i++){
+    amount = filterdata[i].liquidity_BNB;
+    total_liq_bnb += JSON.parse( amount)
+  }
+  let total_value_locked= total_liq_bnb * 304.46;
+  
+    return200(res,
+       { 
+          "msg"                 : "tvl api is working" , 
+          "total_liquidity_BNB" : total_liq_bnb ,  
+          "bnd_price"           : "304.46",
+          "tvl"                 : total_value_locked,
+          "allData"             :allPairsData,
+        });
+  })
+  } catch (error) {
+    return500(res, error);
+  }
+}
+
+
+// const axios = require('axios');
+// import  axios  from 'axios';
+
+//  test(){  
+//   axios.get('https://cashcow-frontend.vercel.app/api/pairs')
+//   .then(res => {
+//     console.log('Status Code:', res.data);
+//   })
+//   .catch(err => {
+//     console.log('Error: ', err.message);
+//   });
+// }
+// test();
--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -5,6 +5,7 @@ export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
     uri: "https://api.thegraph.com/subgraphs/name/jitenrevinfotech/exchange",
+  //  uri: 'https://api.thegraph.com/subgraphs/name/sivarevinfotech/exchange'
   }),
   cache: new InMemoryCache(),
 });
